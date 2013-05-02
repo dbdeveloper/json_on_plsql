1fadd0d733355d0bca8b7fe7ce556e1b12a772f3
Vladyslav Kozlovskyy <dbdeveloper@rambler.ru>
2013-05-02 08:46:21 +0300

    output formatting has been added

--- test.sql
+++ test.sql
@@ -1,3 +1,16 @@
+--------------------------------------------------------------------------------
+-- Copyright (c) 2013 Vladyslav Kozlovskyy
+-- All rights reserved. This program and the accompanying materials
+-- are made available under the terms of the GNU Lesser Public License
+-- which accompanies this distribution, and is available at
+-- http://www.gnu.org/licenses/lgpl.html
+-- 
+-- Contributors:
+--     Vladyslav Kozlovskyy  - dbdeveloper at rambler.ru
+--------------------------------------------------------------------------------
+COLUMN NAME FORMAT A25
+COLUMN VALUE FORMAT A40
+
 select * from table(parse_json('{
    "firstName": "Иван",
    "lastName": "Иванов",
@@ -10,6 +23,9 @@ select * from table(parse_json('{
   }'));
 
 
+prompt ------------------------------------------------------------------------
+
+set linesize 250
 set serveroutput on
 declare
   res pairs;
